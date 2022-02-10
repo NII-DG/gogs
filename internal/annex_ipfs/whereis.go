6aca9694cdd0c46cc34d7123f013b46ab2132ff0
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-02-10 11:03:32 +0900

    whereis.go メソッド作成

--- internal/annex_ipfs/whereis.go
+++ internal/annex_ipfs/whereis.go
@@ -1,7 +1,9 @@
 package annex_ipfs
 
+import "strings"
+
 //git annex whereis --jsonの構造体
-type annexWhereResponse struct {
+type AnnexWhereResponse struct {
 	Command   string   `json:"command"`
 	Note      string   `json:"note"`
 	Success   bool     `json:"success"`
@@ -11,7 +13,45 @@ type annexWhereResponse struct {
 		Here        bool     `json:"here"`
 		Uuid        string   `json:"uuid"`
 		Urls        []string `json:"urls"`
-		Description []string `json:"description"`
+		Description string   `json:"description"`
 	} `json:"whereis"`
 	File string `json:"file"`
+}
+
+type AnnexWhereResponses []AnnexWhereResponse
+type AnnexContentInfo struct {
+	Key  string
+	Hash string
+	File string
+}
+
+func (a AnnexWhereResponse) getKey() string {
+	return a.Key
+}
+
+func (a AnnexWhereResponse) getFile() string {
+	return a.File
+}
+
+func (a AnnexWhereResponse) getHashValueInIPFS() string {
+	whereis := a.Whereis
+	var hash string
+	for _, w := range whereis {
+		if strings.Contains(w.Description, "ipfs") {
+			u := w.Urls[0]
+			index := strings.LastIndex(u, ":")
+			url := &hash
+			*url = u[index+1:]
+		}
+	}
+	return hash
+}
+
+//key,hash,file_nameの3組を返す
+func (a AnnexWhereResponse) getAnnexContentInfo() AnnexContentInfo {
+	info := new(AnnexContentInfo)
+	info.Key = a.getKey()
+	info.Hash = a.getHashValueInIPFS()
+	info.File = a.getFile()
+	return *info
 }
