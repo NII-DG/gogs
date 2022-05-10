4e37a39cd29fb28482ab988e792acecf4f15ad5d
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-05-10 14:36:03 +0900

    resolveAnnexedContentFromIPFS　リファクタリング

--- internal/route/repo/repo_gin_ipfs.go
+++ internal/route/repo/repo_gin_ipfs.go
@@ -17,6 +17,10 @@ import (
 	log "unknwon.dev/clog/v2"
 )
 
+//指定されたコンテンツをIPFSから取得する。この時、ブロックチェーン（BC）における存在証明の検証が行われる。
+//さらに、BCからの取得内容に従い、暗号化コンテンツは復号されて、返される
+//
+//@  contentLocation string　コンテンツロケーション
 func resolveAnnexedContentFromIPFS(c *context.Context, buf []byte, contentLocation string) ([]byte, error) {
 	if !tool.IsAnnexedFile(buf) {
 		// not an annex pointer file; return as is
@@ -31,10 +35,7 @@ func resolveAnnexedContentFromIPFS(c *context.Context, buf []byte, contentLocati
 		log.Info("[Success enable remote(ipfs)] repoPath : %v", repoPath)
 	}
 
-	keyparts := strings.Split(strings.TrimSpace(string(buf)), "/")
-	key := keyparts[len(keyparts)-1]
-
-	//BCAPI通信（コンテンツパスからIPFSハッシュ値を取得）
+	//BCAPI通信（コンテンツパスからBC上のコンテンツ情報を取得）
 	bcContentInfo, err := bcapi.GetContentInfoByLocation(c.User.Name, contentLocation)
 	if err != nil {
 		log.Error("[BC-API HTTP error] %v", err)
@@ -44,64 +45,19 @@ func resolveAnnexedContentFromIPFS(c *context.Context, buf []byte, contentLocati
 
 	//指定のコンテンツの暗号化の有無の判定する。
 	if len(bcContentInfo.FullContentHash) > 0 {
-		//暗号化ファイルの処理
-		if msg, err := git.NewCommand("annex", "copy", "--from", "ipfs", "--key", key).RunInDir(repoPath); err != nil {
-			log.Error("[Failure copy dataObject from ipfs] err : %v, repoPath : %v", err, repoPath)
-		} else {
-			log.Info("[Success copy dataObject from ipfs] key : %v, repoPath : %v, contentlocation: %v, msg", key, repoPath, contentLocation, string(msg))
-		}
-		contentPath, err := git.NewCommand("annex", "contentlocation", key).RunInDir(repoPath)
-		if err != nil {
-			log.Error("Failed to find content location for key %q, err : %v", key, err)
-			c.Data["IsAnnexedFile"] = true
-			return buf, err
-		}
-		log.Trace("contentPath: %v", string(contentPath))
-		contentPath = bytes.TrimSpace(contentPath)
-		filepath := filepath.Join(repoPath, string(contentPath))
-		b, err := ioutil.ReadFile(filepath)
-		if err != nil {
-			log.Error("[ioutil.ReadFile(filepath.Join(repoPath, string(contentPath)))] err : %v, repoPath : %v", err, repoPath)
-		}
-		fullContentHash := string(b)
-
-		//ファイルコンテンツハッシュを比較
-		if fullContentHash != bcContentInfo.FullContentHash {
-			log.Error("[Not match AnnexFullContentHash to BcFullContentHash] FullContentHash : %v, BcFullContentHash : %v", fullContentHash, bcContentInfo.FullContentHash)
-			c.Data["IsAnnexedFile"] = true
-			return buf, fmt.Errorf("[Not math AnnexContentAddress to BcContentAddress]")
-		}
-
-		//ファイルコンテンツハッシュが一致した場合
-		//IPFSから暗号データを取得して、復元する。
-		log.Trace("[Match AnnexFullContentHash to BcFullContentHash] fullContentHash : %v, BCFullContentHash : %v", fullContentHash, bcContentInfo.FullContentHash)
-		//復号ファイルの格納ディレクトリパスの定義
-		dirDecryptedData := strings.Replace(filepath, "objects", "decrypt", 1)
-		err = encyrptfile.Decrypted(bcContentInfo.IpfsCid, c.Repo.Repository.Password, dirDecryptedData)
-		if err != nil {
-			log.Error("[Cannot Decrypting data] File : %v, Error Msg : %v", dirDecryptedData, err)
-			c.Data["IsAnnexedFile"] = true
-			return buf, fmt.Errorf("[Cannot Decrypting data] File : %v, Error Msg : %v", dirDecryptedData, err)
-		}
-		//復号したデータをディレクトリに格納
-		afp, err := os.Open(dirDecryptedData)
-		if err != nil {
-			c.Data["IsAnnexedFile"] = true
-			return buf, err
-		}
-		info, err := afp.Stat()
-		if err != nil {
-			c.Data["IsAnnexedFile"] = true
-			return buf, err
-		}
-		annexDataReader := bufio.NewReader(afp)
-		annexBuf := make([]byte, 1024)
-		n, _ := annexDataReader.Read(annexBuf)
-		annexBuf = annexBuf[:n]
-		c.Data["FileSize"] = info.Size()
-		log.Trace("Annexed file size: %d B", info.Size())
-		return annexBuf, nil
+		//暗号ファイルの処理
+		return resolveEncyrptedContent(c, contentLocation, buf, bcContentInfo)
+	} else {
+		//公開データの処理
+		return resolvePublicContent(c, contentLocation, buf, bcContentInfo)
 	}
+}
+
+func resolvePublicContent(c *context.Context, contentLocation string, buf []byte, bcContentInfo bcapi.ResContentInfo) ([]byte, error) {
+	repoPath := c.Repo.Repository.RepoPath()
+
+	keyparts := strings.Split(strings.TrimSpace(string(buf)), "/")
+	key := keyparts[len(keyparts)-1]
 
 	addressByAnnex, err := GetIpfsHashValueByAnnexKey(key, repoPath)
 	if err != nil {
@@ -151,5 +107,70 @@ func resolveAnnexedContentFromIPFS(c *context.Context, buf []byte, contentLocati
 	log.Trace("Annexed file size: %d B", info.Size())
 	//メモrepopath + /annex を削除する。
 	//gogs-repositories/user1/demo4.git/annex
+	return annexBuf, nil
+}
+
+func resolveEncyrptedContent(c *context.Context, contentLocation string, buf []byte, bcContentInfo bcapi.ResContentInfo) ([]byte, error) {
+	repoPath := c.Repo.Repository.RepoPath()
+
+	keyparts := strings.Split(strings.TrimSpace(string(buf)), "/")
+	key := keyparts[len(keyparts)-1]
+
+	//暗号化ファイルの処理
+	if msg, err := git.NewCommand("annex", "copy", "--from", "ipfs", "--key", key).RunInDir(repoPath); err != nil {
+		log.Error("[Failure copy dataObject from ipfs] err : %v, repoPath : %v", err, repoPath)
+	} else {
+		log.Info("[Success copy dataObject from ipfs] key : %v, repoPath : %v, contentlocation: %v, msg", key, repoPath, contentLocation, string(msg))
+	}
+	contentPath, err := git.NewCommand("annex", "contentlocation", key).RunInDir(repoPath)
+	if err != nil {
+		log.Error("Failed to find content location for key %q, err : %v", key, err)
+		c.Data["IsAnnexedFile"] = true
+		return buf, err
+	}
+	log.Trace("contentPath: %v", string(contentPath))
+	contentPath = bytes.TrimSpace(contentPath)
+	filepath := filepath.Join(repoPath, string(contentPath))
+	b, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Error("[ioutil.ReadFile(filepath.Join(repoPath, string(contentPath)))] err : %v, repoPath : %v", err, repoPath)
+	}
+	fullContentHash := string(b)
+
+	//ファイルコンテンツハッシュを比較
+	if fullContentHash != bcContentInfo.FullContentHash {
+		log.Error("[Not match AnnexFullContentHash to BcFullContentHash] FullContentHash : %v, BcFullContentHash : %v", fullContentHash, bcContentInfo.FullContentHash)
+		c.Data["IsAnnexedFile"] = true
+		return buf, fmt.Errorf("[Not math AnnexContentAddress to BcContentAddress]")
+	}
+
+	//ファイルコンテンツハッシュが一致した場合
+	//IPFSから暗号データを取得して、復元する。
+	log.Trace("[Match AnnexFullContentHash to BcFullContentHash] fullContentHash : %v, BCFullContentHash : %v", fullContentHash, bcContentInfo.FullContentHash)
+	//復号ファイルの格納ディレクトリパスの定義
+	dirDecryptedData := strings.Replace(filepath, "objects", "decrypt", 1)
+	err = encyrptfile.Decrypted(bcContentInfo.IpfsCid, c.Repo.Repository.Password, dirDecryptedData)
+	if err != nil {
+		log.Error("[Cannot Decrypting data] File : %v, Error Msg : %v", dirDecryptedData, err)
+		c.Data["IsAnnexedFile"] = true
+		return buf, fmt.Errorf("[Cannot Decrypting data] File : %v, Error Msg : %v", dirDecryptedData, err)
+	}
+	//復号したデータをディレクトリに格納
+	afp, err := os.Open(dirDecryptedData)
+	if err != nil {
+		c.Data["IsAnnexedFile"] = true
+		return buf, err
+	}
+	info, err := afp.Stat()
+	if err != nil {
+		c.Data["IsAnnexedFile"] = true
+		return buf, err
+	}
+	annexDataReader := bufio.NewReader(afp)
+	annexBuf := make([]byte, 1024)
+	n, _ := annexDataReader.Read(annexBuf)
+	annexBuf = annexBuf[:n]
+	c.Data["FileSize"] = info.Size()
+	log.Trace("Annexed file size: %d B", info.Size())
 	return annexBuf, nil
 }
