82ddb0a9a42c128e7f15c91683441f89f6464385
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2022-05-10 09:53:31 +0900

    methodの説明追記

--- internal/encyrpt_file/aes_ctr_mode.go
+++ internal/encyrpt_file/aes_ctr_mode.go
@@ -10,15 +10,18 @@ import (
 	"unsafe"
 
 	"github.com/NII-DG/gogs/internal/ipfs"
-	log "unknwon.dev/clog/v2"
 )
 
+//AES CTRモードの暗号化メソッド
+//
+//@param filepath　暗号化するファイルのパス
+//
+//@param password 暗号キー
 func Encrypted(filepath, password string) (string, error) {
-	log.Trace("pasword [%v]", password)
 	//原本ファイルの取得
 	plainText, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return "", fmt.Errorf("Cannot find file !!!, TARGET_FILE_PATH : %v", filepath)
+		return "", fmt.Errorf("[Cannot find file !!!, TARGET_FILE_PATH : %v]", filepath)
 	}
 	//共通キーの取得
 	key := []byte(password)
@@ -26,14 +29,14 @@ func Encrypted(filepath, password string) (string, error) {
 	// Create new AES cipher block
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", fmt.Errorf("Failure Creating new AES cipher block in Encrypting This FilePath [%v]", filepath)
+		return "", fmt.Errorf("[Failure Creating new AES cipher block in Encrypting This FilePath : %v]", filepath)
 	}
 
 	// Create IV (cipherText : 暗号化データ)
 	cipherText := make([]byte, aes.BlockSize+len(plainText))
 	iv := cipherText[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return "", fmt.Errorf("Failure Creating new IV in Encrypting This FilePath [%v]", filepath)
+		return "", fmt.Errorf("[Failure Creating new IV in Encrypting This FilePath : %v]", filepath)
 	}
 
 	// Encrypt
